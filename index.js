bd78439fe7c9ac8ee4a3c04aa5a27b8699c8d969
joe <joe@home.com>
2021-07-07 16:22:58 +0100

    Handle misspelled metric command line parameter

--- index.js
+++ index.js
@@ -14,25 +14,29 @@ log4js.configure(constants.loggerConfiguration);
 const logger = log4js.getLogger('index');
 
 const metrics = {
-	cases: cases,
-	deaths: deaths,
-	swabs: swabs,
-	vaccinations: vaccinations,
-	hospitalisations: hospitalisations
+    cases: cases,
+    deaths: deaths,
+    swabs: swabs,
+    vaccinations: vaccinations,
+    hospitalisations: hospitalisations
 };
 
 if (process.argv.length == 3) {
-
-	fs.readFile('./covid19dashboard/data.json', 'utf8', (err, data) => {
-		if (err) {
-	    	logger.error(`Error reading file from disk: ${err}`);
-	  	} else {
-	    	const covidData = JSON.parse(data);
-	    	logger.debug(`Processing ${covidData.length} items`);
-	    	metrics[process.argv[2]].processData(covidData);
-		};
-	});
-
+    const metric = metrics[process.argv[2]];
+    if (metric != undefined) {
+        fs.readFile('./covid19dashboard/data.json', 'utf8', (err, data) => {
+            if (err) {
+                logger.error(`Error reading file from disk: ${err}`);
+            } else {
+                const covidData = JSON.parse(data);
+                logger.debug(`Processing ${covidData.length} items`);
+                metric.processData(covidData);
+            };
+        });
+    } else {
+        logger.info(`Metric "${process.argv[2]}" not found.`);
+        logger.info(`Valid values: [deaths|cases|swabs|vaccinations|hospitalisations]`);
+    }
 } else {
-	logger.info('Usage: node index [deaths|cases|swabs|vaccinations|hospitalisations]');
+    logger.info('Usage: node index [deaths|cases|swabs|vaccinations|hospitalisations]');
 }
