dcf045fe0d66c02cc0920f83d03b3b7afeca8a53
joe <joe@home.com>
2021-07-28 01:10:59 +0100

    Twitter configuration

---
+++ src/twitterConfiguration.js
@@ -0,0 +1,8 @@
+const configuration = {
+  consumer_key: 'consumer_key',
+  consumer_secret: 'consumer_secret',
+  access_token: 'access_token',
+  access_token_secret: 'access_token_secret'
+};
+
+exports.twitterConfiguration = () => configuration;
