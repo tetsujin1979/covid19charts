03e9a946aaeea5c3dad7a4a8888e0cda936109a5
joe <joe@home.com>
2021-07-30 00:12:09 +0100

    Moved webpack to webpack directory create separate config files for lambda and local distributions

--- webpack/base.config.js
+++ webpack/base.config.js
@@ -1,16 +1,24 @@
+const CopyPlugin = require("copy-webpack-plugin");
 const nodeExternals = require('webpack-node-externals');
 const path = require('path');
 
 module.exports = {
   target: 'node',
-  entry: './src/index.js',
   externals: [nodeExternals()],
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, '../dist'),
   },
   mode: 'development',
   optimization: {
     usedExports: true,
-  }
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: "img/", to: "./" },
+        { from: "properties/", to: "./" }
+      ]
+    })
+  ]
 };
