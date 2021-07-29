341a70d80cf7b8f57ebd2ed192b01344c0c64718
joe <joe@home.com>
2021-07-29 10:28:54 +0100

    Moved webpack config into root directory

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
+const nodeExternals = require('webpack-node-externals');
 const path = require('path');
 
 module.exports = {
   target: 'node',
   entry: './src/index.js',
+  externals: [nodeExternals()],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
